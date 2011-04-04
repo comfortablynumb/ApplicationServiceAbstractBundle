0b156573e36db50f1d55bceca2f7e23821f968ef
Gustavo Adrian <comfortablynumb>
2011-04-04 00:55:42 -0300

    Refactor on PersistenceManager component

---
+++ PersistenceManager/ORM/PersistanceManager.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace ENC\Bundle\ApplicationServiceAbstractBundle\PersistenceManager\ORM;
+
+use Doctrine\ORM\EntityManager;
+
+use ENC\Bundle\ApplicationServiceAbstractBundle\PersistenceManager\PersistenceManagerAbstract;
+use ENC\Bundle\ApplicationServiceAbstractBundle\Exception;
+
+class PersistenceManager extends PersistenceManagerAbstract
+{
+    public function setPersistenceManager( EntityManager $entityManager )
+    {
+        $this->pm = $entityManager;
+    }
+
+    public function createQueryBuilder()
+    {
+        return $this->pm->createQueryBuilder();
+    }
+
+    public function lock($object, $lockMode, $lockVersion)
+    {
+        try {
+            $this->pm->lock($object, $lockMode, $lockVersion);
+        } catch (\Doctrine\ORM\OptimisticLockException $e) {
+            $msg = 'Ocurrio un error de concurrencia en la base de datos. Probablemente otra persona envio sus modificaciones sobre la entidad antes de que usted las envie. Por favor, realice sus cambios nuevamente.';
+            
+            throw new Exception\DatabaseConcurrencyException($msg, $e->getCode(), $e);
+        } catch(\Doctrine\ORM\PessimisticLockException $e) {
+            $msg = 'Alguien requirio un bloqueo exclusivo para una de las entidades que usted desea modificar.';
+            
+            throw new Exception\DatabaseConcurrencyException($msg, $e->getCode(), $e);
+        } catch(\Exception $e) {
+            $msg = 'Ocurrio un error desconocido en la aplicacion.';
+            
+            throw new Exception\ApplicationUnknownException($msg, $e->getCode(), $e);
+        }
+    }
+    
+    public function flush(array $options = array())
+    {
+        try {
+            $this->pm->flush($options);
+        } catch(\Doctrine\ORM\OptimisticLockException $e) {
+            $msg = 'Ocurrio un error de concurrencia en la base de datos. Probablemente otra persona envio sus modificaciones sobre la entidad antes de que usted las envie. Por favor, realice sus cambios nuevamente.';
+            
+            throw new Exception\DatabaseConcurrencyException($msg, (int) $e->getCode(), $e);
+        } catch(\Doctrine\ORM\PessimisticLockException $e) {
+            $msg = 'Alguien requirió un bloqueo exclusivo para una de las entidades que usted desea modificar.';
+            
+            throw new Exception\DatabaseConcurrencyException($msg, (int) $e->getCode(), $e);
+        } catch(\PDOException $e) {
+            if ($e->getCode() === '23000')
+            {
+                // Determinamos el codigo del error de la BD
+                $code = $this->extractSqlErrorCodeFromMessage($e->getMessage());
+                
+                switch ($code) {
+                    case '1451':
+                        $msg = 'No se puede eliminar el elemento porque existe/n otro/s elemento/s haciendo referencia a él. ';
+                        $msg .= 'Si aún desea proseguir, por favor, primero elimine el/los elemento/s asociado/s antes de intentar eliminar la entidad seleccionada.';
+                    
+                        throw new Exception\DatabaseConstraintException($msg, (int) $e->getCode(), $e);
+                        
+                        break;
+                    default:
+                        throw new Exception\DatabaseUnknownException('Se produjo un error desconocido en la Base de Datos.', (int) $e->getCode(), $e);
+                        
+                        break;
+                }
+            } else {
+                throw new Exception\DatabaseUnknownException('Se produjo un error desconocido en la Base de Datos.', (int) $e->getCode(), $e);
+            }
+        } catch(\Exception $e) {
+            $msg = 'Ocurrio un error desconocido en la aplicacion.';
+            
+            throw new Exception\ApplicationUnknownException($msg, (int) $e->getCode(), $e);
+        }
+    }
+
+    // Utility Methods
+    public function extractSqlErrorCodeFromMessage($message)
+    {
+        $results = array();
+                
+        preg_match('/SQLSTATE\[[0-9]+\]: [a-zA-Z0-9\s]+: ([0-9]{4})/', $message, $results);
+        
+        return is_array($results) && count($results) > 1 ? $results[1] : '';
+    }
+
+    public function beginTransaction()
+    {
+        $this->getConnection()->beginTransaction();
+    }
+    
+    public function commitTransaction()
+    {
+        $this->getConnection()->commit();
+    }
+    
+    public function rollbackTransaction()
+    {
+        $this->getConnection()->rollback();
+    }
+}
+
+
---
+++ PersistenceManager/PersistenceManagerAbstract.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace ENC\Bundle\ApplicationServiceAbstractBundle\PersistenceManager;
+
+use ENC\Bundle\ApplicationServiceAbstractBundle\Exception;
+
+/**
+ * Abstract PersistanceManager from which ORM and ODM 
+ * PersistanceManager classes must inherit from.
+ *
+ * @author Gustavo Adrian <comfortablynumb@gmail.com
+ */
+class PersistenceManagerAbstract implements PersistenceManagerInterface
+{
+    protected $pm = null;
+    
+    public function __construct($persistenceManager)
+    {
+        $this->setPersistenceManager($persistenceManager);
+    }
+
+    public function clear($objectName)
+    {
+        $this->pm->clear($objectName);
+    }
+
+    public function close()
+    {
+        $this->pm->close();
+    }
+
+    public function contains($object)
+    {
+        return $this->pm->contains($object);
+    }
+
+    public function create($connection, $configuration, $eventManager)
+    {
+        return $this->pm->create($connection, $configuration, $eventManager);
+    }
+
+    public function detach($object)
+    {
+        $this->pm->detach($object);
+    }
+
+    public function getClassMetadata($className)
+    {
+        return $this->pm->getClassMetadata($className);
+    }
+
+    public function getConfiguration()
+    {
+        return $this->pm->getConfiguration();
+    }
+
+    public function getConnection()
+    {
+        return $this->pm->getConnection();
+    }
+
+    public function getEventManager()
+    {
+        return $this->pm->getEventManager();
+    }
+
+    public function getMetadataFactory()
+    {
+        return $this->pm->getMetadataFactory();
+    }
+
+    public function getPartialReference($entityName, $identifier)
+    {
+        return $this->pm->getPartialReference($entityName, $identifier);
+    }
+
+    public function getPersistenceManager()
+    {
+        return $this->pm;
+    }
+
+    public function getProxyFactory()
+    {
+        return $this->pm->getProxyFactory();
+    }
+
+    public function getReference($entityName, $identifier)
+    {
+        return $this->pm->getReference($entityName, $identifier);
+    }
+
+    public function getRepository($objectFullClass)
+    {
+        return $this->pm->getRepository($objectFullClass);
+    }
+
+    public function getUnitOfWork()
+    {
+        return $this->pm->getUnitOfWork();
+    }
+
+    public function merge($object)
+    {
+        return $this->pm->merge($object);
+    }
+
+    public function persist($object)
+    {
+        $this->pm->persist($object);
+    }
+
+    public function refresh($object)
+    {
+        $this->pm->refresh($object);
+    }
+
+    public function remove($object)
+    {
+        $this->pm->remove($object);
+    }
+}
